aaac27baf4ff729799993154e090e8171062d9a4
Amina <amina.muratovic1995@hotmail.com>
2023-01-11 13:49:42 +0100

    added functions

---
+++ aplikacija.js
@@ -0,0 +1,22 @@
+/** @format */
+
+function mojaFunkcija() {
+    document.getElementById("x").innerHTML = "10";
+}
+
+
+function ukiniSliku() {
+    document.getElementById("slidza").style.background = "black";
+}
+
+
+function ukiniSliku() {
+    document.getElementById("slidza2").style.background = "red";
+}
+
+
+function ukiniSliku() {
+    document.getElementById("slidza12").style.background = "white";
+}
+
+
